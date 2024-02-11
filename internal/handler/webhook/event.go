0a18ec4506aec59500e63ec2a7b5f36544d89ffa
murasame <oogiriminister@gmail.com>
2024-02-11 16:44:32 +0900

    add : remove install repo trigger

--- internal/handler/webhook/event.go
+++ internal/handler/webhook/event.go
@@ -69,6 +69,15 @@ func (ge *handler) Installation(e *github.InstallationEvent) error {
 
 func (ge *handler) InstallationRepositories(e *github.InstallationRepositoriesEvent) error {
 	ge.l.Info(fmt.Sprintf("installation repositories event received: %v", e))
+
+	// ctx := context.Background()
+
+	// リポジトリから削除された場合そのラベルを持つDeployたちを削除する
+	if *e.Action == "removed" {
+		for _, repo := range e.RepositoriesRemoved {
+			ge.l.Info(fmt.Sprintf("repository removed: %v", repo))
+		}
+	}
 	return nil
 }
 
