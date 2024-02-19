cb9603a6d86afb89186613e360f89cebd7ff2fdb
murasame29 <oogiriminister@gmail.com>
2024-02-19 14:20:59 +0000

    ファイル命名の変更 , fix typo

---
+++ internal/usecases/application/controller/preview.go
@@ -0,0 +1,61 @@
+package controller
+
+import (
+	"context"
+
+	"github.com/gantrycd/backend/internal/models"
+	v1 "github.com/gantrycd/backend/proto"
+)
+
+// CreatePreviewEnvironmentParams はプレビュー環境を作成するためのパラメータです。
+type CreatePreviewEnvironmentParams struct {
+	Organization string
+	Repository   string
+	PrNumber     string
+	Branch       string
+
+	Config models.PullRequestConfig
+}
+
+func (ge *githubAppEvents) CreatePreviewEnvironment(ctx context.Context, param CreatePreviewEnvironmentParams) (*v1.CreatePreviewReply, error) {
+	// TODO: image buildする
+	image := "nginx:1.16"
+
+	var configs []*v1.Config
+	for _, c := range param.Config.ConfigMaps {
+		configs = append(configs, &v1.Config{
+			Name:  c.Name,
+			Value: c.Value,
+		})
+	}
+
+	// デプロイする
+	return ge.customController.CreatePreview(ctx, &v1.CreatePreviewRequest{
+		Organization:  param.Organization,
+		Repository:    param.Repository,
+		PullRequestId: param.PrNumber,
+		Branch:        param.Branch,
+		Image:         image,
+		Replicas:      "1",
+		Configs:       configs,
+		ExposePorts:   param.Config.ExposePort,
+	})
+}
+
+type DeletePreviewEnvironmentParams struct {
+	Organization string
+	Repository   string
+	PrNumber     string
+	Branch       string
+}
+
+func (ge *githubAppEvents) DeletePreviewEnvironment(ctx context.Context, param DeletePreviewEnvironmentParams) error {
+	_, err := ge.customController.DeletePreview(ctx, &v1.DeletePreviewRequest{
+		Organization:  param.Organization,
+		Repository:    param.Repository,
+		PullRequestId: param.PrNumber,
+		Branch:        param.Branch,
+	})
+
+	return err
+}
