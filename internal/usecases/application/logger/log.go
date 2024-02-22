785555748bce21ac9b50900ea484781b86c5e450
xpadev <xpadev@gmail.com>
2024-02-22 18:58:11 +0900

    fix: grpc error handling

--- internal/usecases/application/logger/log.go
+++ internal/usecases/application/logger/log.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"github.com/gantrycd/backend/internal/models"
 	v1 "github.com/gantrycd/backend/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"io"
 	"log"
 	"net/http"
@@ -36,23 +38,38 @@ func (r *logInteractor) GetLogStream(ctx context.Context, w http.ResponseWriter,
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+	done := make(chan struct{})
 	go func() {
+		defer close(done)
 		for {
 			resp, err := logs.Recv()
-			if err == io.EOF {
-				break
-			}
 			if err != nil {
-				log.Fatalf("ストリームからの受信に失敗しました: %v", err)
+				if err == io.EOF {
+					return
+				}
+				if status.Code(err) == codes.Canceled {
+					return
+				}
+				log.Printf("ストリームからの受信に失敗しました: %v\n", err)
+				return
 			}
 
 			_, err = fmt.Fprintf(w, "data: %s\n\n", resp.Message)
 			if err != nil {
+				log.Printf("データの書き込みに失敗しました: %v\n", err)
 				return
 			}
 			flusher.Flush()
 		}
 	}()
-	<-ctx.Done()
+	select {
+	case <-ctx.Done():
+		if err = logs.CloseSend(); err != nil {
+			log.Printf("ログストリームのクローズに失敗しました: %v\n", err)
+		}
+		<-done
+	case <-done:
+		break
+	}
 	return nil
 }
