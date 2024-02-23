3b99eaaefff15361b5aeba8cd359de76232d42e0
xpadev <xpadev@gmail.com>
2024-02-23 22:41:07 +0900

    k8sclient/feat: config map

--- internal/usecases/core/k8sclient/configMap.go
+++ internal/usecases/core/k8sclient/configMap.go
@@ -14,14 +14,31 @@ func (k *k8sClient) CreateConfigMap(ctx context.Context, namespace string, name 
 			APIVersion: "v1",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "my-config-map",
-			Namespace: "my-namespace",
+			Name:      name,
+			Namespace: namespace,
 		},
 		Data: data,
 	}
 	_, err := k.client.CoreV1().ConfigMaps(namespace).Create(ctx, &configMap, opts)
-	if err != nil {
-		return err
+	return err
+}
+
+func (k *k8sClient) UpdateConfigMap(ctx context.Context, namespace string, name string, data map[string]string, opts metav1.UpdateOptions) error {
+	configMap := v1.ConfigMap{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ConfigMap",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Data: data,
 	}
-	return nil
+	_, err := k.client.CoreV1().ConfigMaps(namespace).Update(ctx, &configMap, opts)
+	return err
+}
+
+func (k *k8sClient) DeleteConfigMap(ctx context.Context, namespace string, name string, opts metav1.DeleteOptions) error {
+	return k.client.CoreV1().ConfigMaps(namespace).Delete(ctx, name, opts)
 }
